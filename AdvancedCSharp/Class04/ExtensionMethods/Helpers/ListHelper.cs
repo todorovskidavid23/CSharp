fc55dec57bb0f7d8dae1c0d902dfdf618c7e5492
David Todorovski <todorovskidavid23@gmail.com>
2026-05-13 20:44:03 +0200

    this ExtenstionMethods

--- AdvancedCSharp/Class04/ExtensionMethods/Helpers/ListHelper.cs
+++ AdvancedCSharp/Class04/ExtensionMethods/Helpers/ListHelper.cs
@@ -4,8 +4,17 @@ using System.Text;
 
 namespace ExtensionMethods.Helpers
 {
-    public class ListHelper
+    public static class ListHelper
     {
-
+        //static metoda se se printa items
+        //extensiot metodot e ust this
+        //ovaa metoda kje se dodade na ovoj tip
+        public static void PrintItems<T>(this List<T> items)//<T> odma posle ova stanuva genericka
+        {
+            foreach (T item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
     }
 }
--- AdvancedCSharp/Class04/ExtensionMethods/Program.cs
+++ AdvancedCSharp/Class04/ExtensionMethods/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using ExtensionMethods.Helpers;
+
+List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
+//List<string> strings = new List<string>();
+List<string> strings = ["str1", "str2", "str3"];
+
+ListHelper.PrintItems<int>(numbers);
+ListHelper.PrintItems<string>(strings);
+//problemot sto imame povtoruvanje
+
+numbers.PrintItems();
+//OVDE NE TREBA PARAMETAR;
+//Ova raboti zaradi THIS
+//dodadi FUNKCIONALONT PrintItems();
